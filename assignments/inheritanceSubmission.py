c8f2262955816435eb978419c34fb35938c136ec
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-09-25 21:28:40 -0700

    inheritance assignment

---
+++ assignments/inheritanceSubmission.py
@@ -0,0 +1,18 @@
+class walker:
+      state = 'undead'
+      brain = 'dead'       #a typical zombie, a roamer, if you will
+      speed = 'slow'
+
+#can you tell what show I'm rewatching right now?
+#a type of walker, rip Hershal's leg
+class lurkers:
+      special_ability = 'jumpscare'
+      appearence = 'fully dead'
+
+#the yuckiest kind of walker
+class bloaters:
+      special_bility = "exploding"
+      appearence = "extreamly bloated"
+
+
+      
