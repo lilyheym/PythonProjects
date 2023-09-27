fb42e3173811f683bb358c3f1284ca7a44013a5f
lilyheym <138812924+lilyheym@users.noreply.github.com>
2023-09-26 18:38:51 -0700

    updated

--- assignments/polymorphismSubmission.py
+++ assignments/polymorphismSubmission.py
@@ -10,21 +10,41 @@ class Vehicle:
             entry_year = input("Enter vehicle year: ")
             entry_make = input("Enter vehicle make: ")
             entry_model = input("Enter vehicle model: ")
-                  print("Sweet! We'll findd a {} {} {} for your as soon as we can!".format(entry_year,entry_make,entry_model))
+            engine_type = input("Enter vehicle engine: ")
+                  print("Sweet! We'll find a {} {} {} with a {} engine for your as soon as we can!".format(entry_year,entry_make,entry_model,engine_type))
             
 
 
 # Child class plane
-class Plane:
+class Plane(Vehicle):
       number_of_wings = 2
       licence_requirement = "Commercial Pilot's Licence"
-      turbine_type = "underwing turbofan" 
+      turbine_type = "underwing turbofan"
+
+      def buyVehicleInfo(self):
+              entry_year = input("Enter vehicle year: ")
+            entry_make = input("Enter vehicle make: ")
+            entry_model = input("Enter vehicle model: ")
+            turbine_type = input("Enter vehicle engine: ")
+                  print("Sweet! We'll find a {} {} {} with {} turbines for your as soon as we can!".format(entry_year,entry_make,entry_model,turbine_type))
+            
+
+            
 
 # Child class boat
-class Boat:
+class Boat(Vehicle):
       boat_type = "commercial fishing"
       water_type = "ocean"
       motor_type = "marine diesel"
+
+      def buyVehicleInfo(self):
+              entry_year = input("Enter vehicle year: ")
+            entry_make = input("Enter vehicle make: ")
+            entry_model = input("Enter vehicle model: ")
+            motor_type = input("Enter vehicle engine: ")
+                  print("Sweet! We'll find a {} {} {} with a {} motor for your as soon as we can!".format(entry_year,entry_make,entry_model,motor_type))
+            
+
       
 
       
